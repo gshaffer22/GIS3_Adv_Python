e2a566a009b5068697cde95072fbc12846720465
gshaffer22 <gshaffer@uoregon.edu>
2025-11-23 17:28:42 -0800

    Add files via upload

---
+++ clip.py
@@ -0,0 +1,3 @@
+# coding: utf-8
+arcpy.analysis.Clip("greenspace", "etobicoke", "greenspace_clip")
+# <Result 'R:\\GEOG491_16440_FALL2025\\Student_Data\\gshaffer\\Week1\\PythonGP\\Results.gdb\\greenspace_clip'>
